e23e51cf3eaec03a6d638595996c99f7f5ba6a0a
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-01-24 11:19:57 -0800

    Initial project setup

---
+++ EntityFrameworkCoreQuery/EntityFrameworkCoreQuery.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCoreQuery.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entity Framework Core Query App");
+        }
+    }
+}
