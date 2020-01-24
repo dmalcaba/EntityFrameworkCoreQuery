9d6ac132983dd0de541208af15a00857e802fe82
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-01-24 15:22:48 -0800

    Add extension method for NoTracking

---
+++ EntityFrameworkCoreQuery/EntityFrameworkCoreQuery.App/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using AdventureWorks.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreQuery.App
+{
+    public static class Extensions
+    {
+        public static void NoTracking(this AdventureWorksDbContext context)
+        {
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+    }
+}
