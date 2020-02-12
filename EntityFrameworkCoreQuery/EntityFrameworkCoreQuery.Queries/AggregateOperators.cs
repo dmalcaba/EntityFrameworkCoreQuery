3187b0b1e8bd52dc258bb75828ac3f2e861a8a2a
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-02-12 08:33:33 -0800

    Added Queries project
    This project will contain sample code for generating common
    SQL queries.

---
+++ EntityFrameworkCoreQuery/EntityFrameworkCoreQuery.Queries/AggregateOperators.cs
@@ -0,0 +1,63 @@
+﻿using AdventureWorks.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkCoreQuery.Queries
+{
+    public class AggregateOperators
+    {
+        public void CountSample()
+        {
+            using (var context = new AdventureWorksDbContext())
+            {
+                var result = context.ProductSubcategory
+                    .Include(x => x.Product)
+                    .Select(x => new { x.Name, x.Product.Count })
+                    .OrderBy(x => x.Name)
+                    .ToList();
+
+                ObjectDumper.Write(result);
+            };
+
+            /*
+            Generates:
+
+            info: Microsoft.EntityFrameworkCore.Database.Command[20101]
+                  Executed DbCommand (32ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+                  SELECT [p0].[Name], (
+                      SELECT COUNT(*)
+                      FROM [Production].[Product] AS [p]
+                      WHERE [p0].[ProductSubcategoryID] = [p].[ProductSubcategoryID]) AS [Count]
+                  FROM [Production].[ProductSubcategory] AS [p0]
+                  ORDER BY [p0].[Name]             
+             */
+        }
+
+        public void CountSampleV2()
+        {
+            using (var context = new AdventureWorksDbContext())
+            {
+                var result = context.Product
+                    .GroupBy(o => o.ProductSubcategory)
+                    .Select(g => new { g.Key.ProductCategory.Name, Count = g.Count() })
+                    .ToList();
+
+                ObjectDumper.Write(result);
+            };
+
+            /*
+            Generates:
+
+            info: Microsoft.EntityFrameworkCore.Database.Command[20101]
+                  Executed DbCommand (32ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+                  SELECT [p0].[Name], (
+                      SELECT COUNT(*)
+                      FROM [Production].[Product] AS [p]
+                      WHERE [p0].[ProductSubcategoryID] = [p].[ProductSubcategoryID]) AS [Count]
+                  FROM [Production].[ProductSubcategory] AS [p0]
+                  ORDER BY [p0].[Name]             
+             */
+        }
+    }
+}
