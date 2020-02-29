4c97d45ba3938b829a806af16c3e9a466bdf23b7
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-02-28 16:09:34 -0800

    Cleanup Program.cs

--- EntityFrameworkCoreQuery/EntityFrameworkCoreQuery.App/Program.cs
+++ EntityFrameworkCoreQuery/EntityFrameworkCoreQuery.App/Program.cs
@@ -1,7 +1,5 @@
-﻿using AdventureWorks.Data.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using EntityFrameworkCoreQuery.Queries;
 using System;
-using System.Linq;
 
 namespace EntityFrameworkCoreQuery.App
 {
@@ -10,13 +8,6 @@ namespace EntityFrameworkCoreQuery.App
         static void Main(string[] args)
         {
             Console.WriteLine("Entity Framework Core Query App");
-
-            using var context = new AdventureWorksDbContext();
-            context.NoTracking();
-
-            var result = context.ProductSubcategory
-                .Include(x => x.ProductCategory)
-                .Take(2).ToList();
         }
     }
 }
